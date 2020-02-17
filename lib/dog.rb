8305c78f115801e7e83ffbe04d091e527bf49185
Kyle Talmage <ktalmage85@gmail.com>
2020-02-17 17:25:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog #creates class definition 
+  
+  # adds an instance method to our class
+  def bark
+    puts "Woof!"
+  end
+  #adds sit instance to the the Dog class
+  def sit
+    puts "The Dog is sitting"
+  end
+end
